7490ca923391f16569dd8c9871f0f73e374b26b8
chuthulhu <misterakichu@gmail.com>
2025-11-29 00:39:04 +0900

    docs: 다양한 가이드 문서 및 테스트 프레임워크 파일 추가, Netlify 설정 변경 및 패키지 의존성 업데이트

--- class-site/functions/api/test.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-    return new Response("Functions are working!", {
-        status: 200,
-        headers: {
-            "Content-Type": "text/plain"
-        }
-    });
-}
